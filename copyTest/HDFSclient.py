86fb444e9a051fbb01b7ce0b18d2951b3bd2bc97
Tobias Sørensen <tsoer18@gmail.com>
2023-12-20 14:24:57 +0100

    8020

--- copyTest/HDFSclient.py
+++ copyTest/HDFSclient.py
@@ -1,8 +1,8 @@
 from hdfs import InsecureClient
 
 HDFS_HOSTS: list[str] = [
-    "http://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:9870",
-    "http://simple-hdfs-namenode-default-1.simple-hdfs-namenode-default:9870",
+    "http://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:8020",
+    "http://simple-hdfs-namenode-default-1.simple-hdfs-namenode-default:8020",
 ]
 HDFS_USER_NAME: str = "hdfs"
 
