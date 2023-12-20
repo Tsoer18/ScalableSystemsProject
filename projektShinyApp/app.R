031c79d7f5a8db1c4d9cb2d2da0eb43e9bbb788d
Tobias Sørensen <tsoer18@gmail.com>
2023-12-20 16:15:51 +0100

    running shiny

--- projektShinyApp/app.R
+++ projektShinyApp/app.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 
- data <- read.df("hdfs://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:8020/weather-report.avro", "avro")
+ #data <- read.df("hdfs://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:8020/weather-report.avro", "avro")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
