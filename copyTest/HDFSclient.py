ecc918c32a899f2997dbeb0fcfbe9a978e1430e2
Tobias Sørensen <tsoer18@gmail.com>
2023-12-20 14:43:09 +0100

    resetting changes

--- copyTest/HDFSclient.py
+++ copyTest/HDFSclient.py
@@ -1,10 +1,10 @@
 from hdfs import InsecureClient
 
 HDFS_HOSTS: list[str] = [
-    "http://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:8020",
-    "http://simple-hdfs-namenode-default-1.simple-hdfs-namenode-default:8020",
+    "http://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:9870",
+    "http://simple-hdfs-namenode-default-1.simple-hdfs-namenode-default:9870",
 ]
-HDFS_USER_NAME: str = "hdfs"
+HDFS_USER_NAME: str = "stackable"
 
 
 def get_hdfs_client() -> InsecureClient:
