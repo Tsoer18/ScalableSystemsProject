74e0dee7303fda3f2cd9b3f716ec4dd0ac014e69
oliverrise <oliverrise@hotmail.com>
2023-12-20 18:21:19 +0100

    added more data and made it look through ever file in data directory

--- copyTest/simple-producer-tweets.py
+++ copyTest/simple-producer-tweets.py
@@ -1,13 +1,18 @@
 import bz2
 import json
+import os
 from client import get_producer, DEFAULT_TOPIC, produce_msg, send_msg
 
 
 def main():
-    with bz2.open('data/0.json.bz2') as bzinput:
-        for i, line in enumerate(bzinput):
-            tweets = json.loads(line)
-            send_msg(key = tweets['data']['created_at'], value = tweets['data']['text'], topic = "INGESTION_TWEETS", producer = get_producer())   
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):
+            print(f)
+        with bz2.open(f) as bzinput:
+            for i, line in enumerate(bzinput):
+                tweets = json.loads(line)
+                send_msg(key = tweets['data']['created_at'], value = tweets['data']['text'], topic = "INGESTION_TWEETS", producer = get_producer())   
 
 if __name__ == "__main__":
     main()
