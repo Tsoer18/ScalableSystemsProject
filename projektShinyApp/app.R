9033a99e2a9f8a8b880a12f95dd28c4d06b1ef0b
oliverrise <oliverrise@hotmail.com>
2023-12-22 10:16:22 +0100

    Tried fixing path

--- projektShinyApp/app.R
+++ projektShinyApp/app.R
@@ -10,14 +10,15 @@
 library(shiny)
 library(RJDBC)
 
-avro_driver <- JDBC(driverClass = "cdata.jdbc.avro.AvroDriver", classPath = "AvroJDBCDriver\\lib\\cdata.jdbc.avro.jar", identifier.quote = "'")
+avro_driver <- JDBC(driverClass = "cdata.jdbc.avro.AvroDriver", classPath = "AvroJDBCDriver/lib/cdata.jdbc.avro.jar", identifier.quote = "'")
 avro_conn <- dbConnect(avro_driver,"jdbc:avro:URI=hdfs://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:8020/tweets.avroInitiateOAuth=GETANDREFRESH")
 dbListTables(avro_conn)
 #SKAL BRUGE CDATA DRIVER SOM SKAL DOWNLOADES OG SMIDES OVER PÅ VM POTENTIELT IDK HVORDAN DET VIRKER XD
 #
-kafka_driver <- JDBC(driverClass = "cdata.jdbc.apachekafka.ApacheKafkaDriver", classPath = "ApacheKafkaJDBCDriver\\lib\\cdata.jdbc.apachekafka.jar", identifier.quote = "'")
+kafka_driver <- JDBC(driverClass = "cdata.jdbc.apachekafka.ApacheKafkaDriver", classPath = "ApacheKafkaJDBCDriver/lib/cdata.jdbc.apachekafka.jar", identifier.quote = "'")
 kafka_conn <- dbConnect(kafka_driver,"jdbc:apachekafka:BootStrapServers=https://strimzi-kafka-bootstrap:9092;Topic=INGESTION_TWEETS;")
 dbListTables(kafka_conn)
+
 
 #data <- read.df("hdfs://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:8020/weather-report.avro", "avro")
 
