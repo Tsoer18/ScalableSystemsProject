cfcb9c59fe8340096b74ab0dd6d41953878022b7
OliverRise12 <72047068+OliverRise12@users.noreply.github.com>
2023-12-23 10:32:35 +0100

    qeq

--- copyTest/pyHDFS_reader_temperature.py
+++ copyTest/pyHDFS_reader_temperature.py
@@ -10,13 +10,21 @@ import os
 
 client = get_hdfs_client()
 with AvroReader(client, "/weather-report.avro") as reader:
-        
+        rows = []
         counter = 0
+        filename = "persistentTemperature.csv"
         # Print a list of the data"
         for x in list(reader):
                 print(x["date"])
                 print(x["temperature"])
-                print(counter)
-                counter +=1
+                x["temperature"].replace("\"","")
+                print(x["temperature"])
+                row = [x["date"], x["temperature"]]
+                rows.append(row)
+        with open(filename, 'w') as csvfile: 
+                for row in rows:
+                        csvwriter = csv.writer(csvfile)
+                        csvwriter.writerow(row) 
+                csvfile.close()
         
 
