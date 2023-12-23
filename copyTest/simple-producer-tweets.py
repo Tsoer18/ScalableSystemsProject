bd4a9ddece9660d84b898ae767725b1c74aa0942
OliverRise12 <72047068+OliverRise12@users.noreply.github.com>
2023-12-23 19:55:22 +0100

    Added measurements

--- copyTest/simple-producer-tweets.py
+++ copyTest/simple-producer-tweets.py
@@ -2,9 +2,14 @@ import bz2
 import json
 import os
 from client import get_producer, DEFAULT_TOPIC, produce_msg, send_msg
+import time
 directory = 'data'
+counter = 0
+measurements = []
+counter2 = 0
 
 def main():
+    tic = time.perf_counter
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         if os.path.isfile(f):
@@ -12,9 +17,17 @@ def main():
         with bz2.open(f) as bzinput:
             for i, line in enumerate(bzinput):
                 tweets = json.loads(line)
-                print(tweets['data']['created_at'] + tweets['data']['text'])
-                
+                print(tweets['data']['created_at'], tweets['data']['text'])
                 send_msg(key = tweets['data']['created_at'], value = tweets['data']['text'], topic = "INGESTION_TWEETS", producer = get_producer())   
+                counter += 1
+                if counter == 100 & counter2 < 11:
+                    toc = time.perf_counter
+                    measurements.append[tic-toc]
+                    tic = time.perf_counter
+                    counter = 0
+                    counter2 += 1
+                if counter == 100 & counter2 >= 11:
+                    print(measurements)
 
 if __name__ == "__main__":
     main()
