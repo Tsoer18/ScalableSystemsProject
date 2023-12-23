6c8ead75014cfab927550737bd92386a03bd2ec9
OliverRise12 <72047068+OliverRise12@users.noreply.github.com>
2023-12-23 20:10:46 +0100

    yes

--- copyTest/simple-producer.py
+++ copyTest/simple-producer.py
@@ -1,5 +1,6 @@
 import csv
 import threading
+import time
 from client import get_producer, DEFAULT_TOPIC, produce_msg, send_msg
 
 
@@ -11,12 +12,30 @@ class RepeatTimer(threading.Timer):
 
 def main():
     rows = []
+    counter = 0
+    measurements = []
+    counter2 = 0
+    tic = time.perf_counter
     with open("GlobalLandTemperaturesByCity.csv", 'r') as file:
         csvreader = csv.reader(file)
         header = next(csvreader)
         for row in csvreader:
             print(row[0], row[1])
-            send_msg(key = row[0], value = row[1], topic = "INGESTION", producer = get_producer())   
+            send_msg(key = row[0], value = row[1], topic = "INGESTION", producer = get_producer())
+            counter += 1
+            if counter == 100 & counter2 < 11:
+                toc = time.perf_counter
+                measurements.append[tic-toc]
+                tic = time.perf_counter
+                print("DID ANOTHER MEASUREMENT")
+                print(counter2)
+                counter = 0
+                counter2 += 1
+            if counter == 100 & counter2 >= 11:
+                print("DONE WITH MEASUREMENTS")
+                print(measurements)
+                print("_____________________________________________________")
+                time.sleep(120)  
 
 if __name__ == "__main__":
     main()
