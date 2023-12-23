92d8a78c6187401135468d558fb1b7af594d1b9a
OliverRise12 <72047068+OliverRise12@users.noreply.github.com>
2023-12-23 10:09:54 +0100

    asd

--- copyTest/client.py
+++ copyTest/client.py
@@ -48,8 +48,10 @@ def recive_msg(consumer: KafkaConsumer, avro_file_path) -> None:
             with AvroWriter(client, avro_file_path,overwrite=True) as writer:
                 with AvroReader(client, "/weather-report.avro") as reader:
                     print("Current data in the reader is:")
-                    print((dict(reader)))
+                    print((list(reader)))
                     print("-----------------")
+                    for data in list(reader):
+                        print (data)
                 writer.write({"date": msg.key.decode(DEFAULT_ENCODING), "temperature" : msg.value.decode(DEFAULT_ENCODING)})
                 print("Wrote to file")
         if avro_file_path == "/tweets.avro":
