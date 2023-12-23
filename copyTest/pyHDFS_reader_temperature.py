d51b3e9b11546f19a302826bbe22b8bfc5daa827
OliverRise12 <72047068+OliverRise12@users.noreply.github.com>
2023-12-23 14:17:19 +0100

    ny persistent header

--- copyTest/pyHDFS_reader_temperature.py
+++ copyTest/pyHDFS_reader_temperature.py
@@ -21,7 +21,7 @@ with AvroReader(client, "/weather-report.avro") as reader:
         if (os.path.isfile(filename) != True):
                 with open(filename, 'w') as csvfile: 
                         csvwriter = csv.writer(csvfile)
-                        header = ["date","temperature"]
+                        header = ['date','temperature']
                         csvwriter.writerow(header) 
                         csvfile.close()
         with open(filename, 'w') as csvfile: 
